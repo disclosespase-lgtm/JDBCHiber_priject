bfc7062939103f2313220573f89aa4bb38c0c6be
disclosespase-lgtm <disclose.spase@gmail.com>
2026-05-17 12:16:52 +0500

    JDBC Pravki 3.0

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,9 +3,11 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
+@Slf4j
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
@@ -16,11 +18,11 @@ public class Main {
         userService.createUsersTable();
 
         //добавили человеков 5шт
-        userService.saveUser("Сергей", "Федоренко",  (byte) 20);
-        userService.saveUser("Матвей", "Пипколипко",  (byte) 13);
-        userService.saveUser("Иван", "Кокушкин",  (byte) 62);
-        userService.saveUser("Ригина", "Наморозильникова",  (byte) 43);
-        userService.saveUser("Клавдия", "Муравешкина",  (byte) 30);
+        userService.saveUser("Сергей", "Федоренко", (byte) 20);
+        userService.saveUser("Матвей", "Пипколипко", (byte) 13);
+        userService.saveUser("Иван", "Кокушкин", (byte) 62);
+        userService.saveUser("Ригина", "Наморозильникова", (byte) 43);
+        userService.saveUser("Клавдия", "Муравешкина", (byte) 30);
 
         //Вывели лист в консольку
         List<User> listUsers = userService.getAllUsers();
